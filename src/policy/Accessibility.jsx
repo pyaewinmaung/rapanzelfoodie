708230cb9527ab1445dcad2980cfe2a8c3dbb017
Pyae Win Maung <pyaewinmaung2021@gmail.com>
2024-01-01 10:43:21 +0630

    Policy

---
+++ src/policy/Accessibility.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Accessibility = () => {
+  return (
+    <div>
+        <p>
+            FoodieRapanzel - Accessibility Policy
+
+            Last Updated: [1.1.2024]
+
+            1 Commitment to Accessibility 
+                - FoodieRapanzel is committed to providing an accessible experience for all users, including those with disabilities.
+
+            2. Accessibility Standards 
+                - Our website aims to comply with applicable accessibility standards, including the Web Content Accessibility Guidelines (WCAG) 2.1.
+
+            3. Accessible Design and Features 
+                - FoodieRapanzel incorporates accessible design principles to ensure a positive user experience for individuals with disabilities.
+                - Features such as navigation, forms, and multimedia are designed to be accessible and user-friendly.
+
+            4. Assistive Technologies Compatibility 
+                - We strive to ensure compatibility with common assistive technologies, such as screen readers and voice recognition software.
+
+            5. Testing and Evaluation 
+                - Regular accessibility testing and evaluation are conducted to identify and address potential accessibility barriers.
+
+            6. User Feedback 
+                - We welcome feedback from users, including those with disabilities, on how we can improve accessibility. Please contact [Your Contact Information] with your feedback.
+
+            7. Accessible Content 
+                - Content published on FoodieRapanzel is created with accessibility in mind. We aim to provide content in various formats to accommodate different needs.
+
+            8. Third-Party Content 
+                - We encourage accessibility in third-party content, and we seek to collaborate with providers that share our commitment to inclusivity.
+
+            9. Continuous Improvement 
+                - FoodieRapanzel is committed to continuous improvement in accessibility. We regularly review our practices and update our website to enhance accessibility.
+
+            10. Contact Information 
+                - For questions, concerns, or assistance related to website accessibility, please contact [Your Contact Information].
+
+        </p>
+    </div>
+  )
+}
+
+export default Accessibility
---
+++ src/policy/Privacy.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Privacy = () => {
+  return (
+    <div>
+        FoodieRapanzel- Privacy Policy 
+        Last Updated: [1.1.2024]
+        1.Information We Collect 
+        -  Personal Information:  When users create an account, submit recipes, or interact with the website, we may collect personal information such as names, email addresses, and profile details.
+        -  Non-Personal Information:  We may collect non-personal information, such as browser type, IP address, and usage data, to improve our website.
+
+        2.How We Use Information 
+        -  Personalization:  We use collected information to personalize user experience, including recommending recipes based on preferences.
+        -  Communication:  We may use email addresses to communicate important updates, newsletters, and promotions.
+        -  Improvements:  Collected data helps us analyze user behavior and improve our website and services.
+
+        3.Cookies and Tracking Technologies 
+        - We use cookies and similar technologies to enhance user experience and collect information about usage patterns.
+        - Users can adjust browser settings to refuse cookies, but it may affect website functionality.
+
+        4.Third-Party Links and Services 
+        - Our website may contain links to third-party websites. We are not responsible for the privacy practices of these sites.
+
+        5.Data Security 
+        - We implement reasonable security measures to protect user data; however, no method of transmission is 100% secure.
+
+        6.User Control and Opt-Out 
+        - Users can update their account information and preferences at any time.
+        - Users can opt-out of promotional emails by following the instructions in the emails.
+
+        7.Children&apos;s Privacy 
+        - Our website is not intended for children under the age of 13. We do not knowingly collect information from children.
+
+        8.Changes to Privacy Policy 
+        - We reserve the right to update our Privacy Policy. Users will be notified of any significant changes.
+
+        9.Contact Information 
+        - For questions or concerns regarding our Privacy Policy, contact [Your Contact Information].
+    </div>
+  )
+}
+
+export default Privacy
